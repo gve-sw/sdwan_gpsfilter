12f20f1b05a2aba766d8215689cf93308f05d796
Hafida Barhoun <hbarhoun@cisco.com>
2019-05-02 18:48:10 +0200

    Cleaning code

--- tony_project/geo_app/API/GPS_filtering.py
+++ tony_project/geo_app/API/GPS_filtering.py
@@ -13,25 +13,15 @@ def map_filter(country):
     
     # Retrieve Device GPS locations
     GPS = get_GPS()
-    
-    ctx = ssl.create_default_context(cafile=certifi.where())
-    geopy.geocoders.options.default_ssl_context = ctx
-    geopy.geocoders.options.default_user_agent = "my-application"
-    geolocator = Nominatim()
 
     map = {}
 
     for device in GPS:
-		# print("Device : " + location + " GPS: " + GPS[location]['GPS'])
         
         coordinates = (GPS[device]['lat'], GPS[device]['lon'])
-        
-		# loc = geolocator.reverse(coordinates)
 
         loc = rg.search(coordinates)
         country = loc[0]['cc']
-        print("Country")
-        print(country)
 
         if country == user_loc:
             map.update ({device: {'Country': country, 'GPS' : coordinates}})
