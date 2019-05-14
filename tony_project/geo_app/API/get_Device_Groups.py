134066f9696e5103cb26103d78aeb0db79614195
Hafida <hbarhoun@cisco.com>
2019-05-14 16:26:48 +0200

    Device group feature

--- tony_project/geo_app/API/get_Device_Groups.py
+++ tony_project/geo_app/API/get_Device_Groups.py
@@ -1,7 +1,17 @@
 from .api_GET import api_get
 
-def api_device_group():
+def api_device_group(region):
+	response = api_get("dataservice/group/devices?groupId=" + region)
+	data = response['data']
 
-    # Retrieve Devices Groups
-    data = api_get("dataservice/group/device")
-    return(data)
+	map = {}
+
+	for item in data:
+		hostname = item['host-name']
+		device_type = item['device-model']
+		latitude = float(item['latitude'])
+		longitude = float(item['longitude'])
+		coordinates = (latitude, longitude)
+		map.update ({hostname: {'Type': device_type, 'lat' : latitude, 'lon' : longitude}})
+
+	return(map)
--- tony_project/geo_app/API/main.py
+++ tony_project/geo_app/API/main.py
@@ -1,6 +1,4 @@
-from .get_Device_Groups import api_device_group
-from .get_Device_GPS import get_GPS
-from .GPS_filtering import map_filter
+from get_Device_Groups import api_device_group
 import json
 
 if __name__ == '__main__':
@@ -11,7 +9,7 @@ if __name__ == '__main__':
     # Retrieve Device GPS locations
     #GPS = get_GPS()
 
-    display = map_filter("US")
+    display = api_device_group("europe")
 
     # Json.dumps method converts the dictionary it into a JSON string for a prettier display
     pretty_json = (json.dumps(display, indent=3, sort_keys=True))
